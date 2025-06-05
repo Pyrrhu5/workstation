3d70ed23d81ce6dd6d399174ee91f13390166fca
Pyrrhu5 <67189832+Pyrrhu5@users.noreply.github.com>
2025-06-05 22:25:27 +0200

    Refactor for cleaner code

--- workstation/workstation.ino
+++ workstation/workstation.ino
@@ -22,8 +22,8 @@ U8G2_SSD1306_128X32_UNIVISION_F_HW_I2C u8g2(U8G2_R0);
 #define DT 3
 
 // Menu options
-const char* menu_options[] = { "first option", "second option", "third option", "fourth option", "fifth option" };
-const int menu_size = sizeof(menu_options) / sizeof(menu_options[0]);
+const char* menuOptions[] = { "first option", "second option", "third option", "fourth option", "fifth option" };
+const int menuSize = sizeof(menuOptions) / sizeof(menuOptions[0]);
 const int detentSensitivity = 2; // how sensitive is the rotary encoder
 
 // Control variables
@@ -50,44 +50,73 @@ void show_menu(int center_index) {
 
   // Previous item
   u8g2.setFont(u8g2_font_6x10_tr);
-  u8g2.drawStr(0, 10, menu_options[(center_index - 1 + menu_size) % menu_size]);
+  u8g2.drawStr(0, 10, menuOptions[(center_index - 1 + menuSize) % menuSize]);
 
   // Selected item
   u8g2.setFont(u8g2_font_9x15_tr);
-  u8g2.drawStr(0, 23, menu_options[center_index]);
+  u8g2.drawStr(0, 23, menuOptions[center_index]);
 
   // Next item
   u8g2.setFont(u8g2_font_6x10_tr);
-  u8g2.drawStr(0, 32, menu_options[(center_index + 1) % menu_size]);
+  u8g2.drawStr(0, 32, menuOptions[(center_index + 1) % menuSize]);
 
   u8g2.sendBuffer();
 }
 
-void loop() {
+enum Rotation {
+  NO_ROTATION = 0,
+  CLOCKWISE = 1,
+  COUNTERCLOCKWISE = -1
+};
+
+// Fix Arduino compiler not recognizing custom types
+// So, just prototype it... Life is good
+Rotation rotation_event();
+void on_rotation(Rotation rotationEvent);
+
+Rotation rotation_event() {
+  /* Detects if the rotary encoder rotated. */
   int currentCLKState = digitalRead(CLK);
   int currentDTState = digitalRead(DT);
 
-  if (currentCLKState != lastCLKState) {
-    if (currentCLKState == LOW) {
-      if (currentDTState != currentCLKState) {
-        pulseCounter++;
-      } else {
-        pulseCounter--;
-      }
-
-      if (pulseCounter >= detentSensitivity) {
-        shift++;
-        pulseCounter = 0;
-        shift = (shift % menu_size + menu_size) % menu_size;
-        show_menu(shift);
-      }
-      else if (pulseCounter <= -detentSensitivity) {
-        shift--;
-        pulseCounter = 0;
-        shift = (shift % menu_size + menu_size) % menu_size;
-        show_menu(shift);
-      }
-    }
+  if (currentCLKState == lastCLKState || currentCLKState != LOW) {
+    lastCLKState = currentCLKState;
+    return NO_ROTATION;
   }
+
   lastCLKState = currentCLKState;
+
+  return (currentDTState != currentCLKState) ? COUNTERCLOCKWISE : CLOCKWISE;
+}
+
+void on_rotation(Rotation rotationEvent){
+  /* Handles rotation of the rotary encoder */
+  switch (rotationEvent) {
+    case CLOCKWISE:
+      pulseCounter++;
+      break;
+    case COUNTERCLOCKWISE:
+      pulseCounter--;
+      break;
+  }
+
+  if (pulseCounter >= detentSensitivity) {
+    shift++;
+    pulseCounter = 0;
+  }
+  else if (pulseCounter <= -detentSensitivity) {
+    shift--;
+    pulseCounter = 0;
+  }
+
+  shift = (shift % menuSize + menuSize) % menuSize;
+  show_menu(shift);
+}
+
+
+void loop() {
+  Rotation rotationEvent = rotation_event();
+  if (rotationEvent != NO_ROTATION) {
+    on_rotation(rotationEvent);
+  }
 }
