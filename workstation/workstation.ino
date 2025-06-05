7638ece5fe9df149c1a17976b3aec648112846e8
Pyrrhu5 <67189832+Pyrrhu5@users.noreply.github.com>
2025-06-05 21:43:05 +0200

    Remove interrupt

--- workstation/workstation.ino
+++ workstation/workstation.ino
@@ -26,16 +26,15 @@ const char* menu_options[] = { "first option", "second option", "third option", 
 const int menu_size = sizeof(menu_options) / sizeof(menu_options[0]);
 
 // Control variables
-volatile int shift = 0;
-volatile bool rotated = false;
-int lastCLK = 0;
+int shift = 0;
+int lastCLKState = 0;
+int lastDTState = 0;
 
 void setup_encoder() {
   pinMode(CLK, INPUT);
   pinMode(DT, INPUT);
-  lastCLK = digitalRead(CLK);
-  // Register on rotary encoder rotation event
-  attachInterrupt(digitalPinToInterrupt(CLK), update_shift, CHANGE);
+  lastCLKState = digitalRead(CLK);
+  lastDTState = digitalRead(DT);
 }
 
 void setup() {
@@ -61,34 +60,21 @@ void show_menu(int center_index) {
   u8g2.sendBuffer();
 }
 
-void update_shift() {
-  int currentCLK = digitalRead(CLK);
-  if (currentCLK != lastCLK) {
-    if (digitalRead(DT) != currentCLK) {
-      shift++;
-    } else {
-      shift--;
-    }
-    rotated = true;
-  }
-  lastCLK = currentCLK;
-}
-
 void loop() {
-  static int displayed_shift = -1;
+  int currentCLKState = digitalRead(CLK);
+  int currentDTState = digitalRead(DT);
 
-  // Bounce protection
-  if (rotated) {
-    noInterrupts();
-    int local_shift = shift;
-    rotated = false;
-    interrupts();
+  if (currentCLKState != lastCLKState) {
+    if (currentCLKState == LOW) {
+      if (currentDTState != currentCLKState) {
+        shift++;
+      } else {
+        shift--;
+      }
 
-    local_shift = (local_shift % menu_size + menu_size) % menu_size;
-
-    if (local_shift != displayed_shift) {
-      show_menu(local_shift);
-      displayed_shift = local_shift;
+      shift = (shift % menu_size + menu_size) % menu_size;
+      show_menu(shift);
     }
   }
+  lastCLKState = currentCLKState;
 }
