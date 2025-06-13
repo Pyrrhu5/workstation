fa54642aaf772c1fa2f1d877e8b032500c707b20
Aymeric <aymeric.deschard@disroot.org>
2025-06-13 12:57:06 +0200

    Add IR remote

--- workstation/remotes.cpp
+++ workstation/remotes.cpp
@@ -1,5 +1,16 @@
 #include <Arduino.h>
+#include <IRremote.hpp>
 #include "remotes.h"
+
+
+constexpr uint8_t ButtonRemote::BUTTON_PINS[];
+
+void ButtonRemote::begin() {
+    for (int i = 0; i < PIN_COUNT; i++){
+		pinMode(BUTTON_PINS[i], OUTPUT);
+		digitalWrite(BUTTON_PINS[i], HIGH);
+	}
+}
 
 
 void ButtonRemote::press_button(uint8_t button_number) {
@@ -8,4 +19,15 @@ void ButtonRemote::press_button(uint8_t button_number) {
         delay(DELAY_MS);
         digitalWrite(BUTTON_PINS[button_number], HIGH);
     } // Error handling is too mainstream
+}
+
+
+constexpr uint8_t IRRemote::IR_COMMANDS[];
+
+void IRRemote::begin() {
+    IrSender.begin(IR_PIN);
+}
+
+void IRRemote::press_button(uint8_t button_number) {
+	IrSender.sendNEC(DEVICE_ADDRESS, IR_COMMANDS[button_number], COMMAND_REPEAT);
 }
--- workstation/remotes.h
+++ workstation/remotes.h
@@ -13,8 +13,7 @@ class ButtonRemote{
      * Check with a voltmeter which pad of the circuit board is which:
      * Ground indicates 0, the control should up/down when a button is pressed
      * 
-     * 
-    */
+     */
 private:
     // This assumes that the first item is button 1, the second, 2, etc.
     static constexpr uint8_t BUTTON_PINS[] = {A0, A1, A2, A3};
@@ -23,7 +22,28 @@ private:
     // if press_button does not affect the remote controller
     static constexpr uint8_t DELAY_MS = 100;
 public:
+    static void begin();
     static void press_button(uint8_t button_number);
 };
+
+class IRRemote{
+    /* Represents a remote control manipulated by infrared.
+     *
+     * This has been tailored for Aten's vs481b, the IR codes might be different.
+     *
+     */
+private:
+    static constexpr uint8_t IR_COMMANDS[] = {0x4, 0x5, 0x6, 0x1E};
+    static constexpr size_t CMD_COUNT = sizeof(IR_COMMANDS) / sizeof(IR_COMMANDS[0]);
+    // The address of the target device
+    static constexpr uint8_t DEVICE_ADDRESS = 0x0;
+    // How many times a command should be sent
+    static constexpr uint8_t COMMAND_REPEAT = 0;
+    static constexpr uint8_t IR_PIN = 11;
+public:
+    static void begin();
+    static void press_button(uint8_t button_number);
+};
+
 
 #endif
