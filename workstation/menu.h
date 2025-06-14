f9506612693154a437b5e2b559e76282d54ebe76
Pyrrhu5 <67189832+Pyrrhu5@users.noreply.github.com>
2025-06-14 16:46:11 +0200

    Menu as a class

--- workstation/menu.h
+++ workstation/menu.h
@@ -1,8 +1,19 @@
 #ifndef MENU_H
 #define MENU_H
+#include <Wire.h>
+#include <U8g2lib.h>
 
 
-void setup_menu();
-void show_menu(int center_index);
+class Menu{
+    private:
+        static constexpr const char* menuOptions[] = { "first option", "second option", "third option", "fourth option", "fifth option" };
+        static constexpr uint8_t menuSize = sizeof(menuOptions) / sizeof(menuOptions[0]);
+        static U8G2_SSD1306_128X32_UNIVISION_F_HW_I2C u8g2;
+
+    public:
+        static void  show(int center_index);
+        static void begin();
+};
+
 
 #endif
