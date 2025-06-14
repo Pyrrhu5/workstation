4a94995a5887e6b6834f942094c8ee2584fc546b
Pyrrhu5 <67189832+Pyrrhu5@users.noreply.github.com>
2025-06-14 17:04:27 +0200

    Menu has multiple options at once

--- workstation/menu.cpp
+++ workstation/menu.cpp
@@ -4,24 +4,46 @@
 #include <U8g2lib.h>
 #include "menu.h"
 
-constexpr const char* Menu::menuOptions[];
+constexpr const char* Menu::menuText[];
 constexpr uint8_t Menu::menuSize;
+constexpr uint8_t Menu::menuOptions[][2];
 
 U8G2_SSD1306_128X32_UNIVISION_F_HW_I2C Menu::u8g2(U8G2_R0);
+
+void Menu::formatMenuOption(uint8_t index, char* buffer, size_t bufferSize) {
+  uint8_t first = Menu::menuOptions[index][0];
+  uint8_t second = Menu::menuOptions[index][1];
+
+  if (second == 255) {
+    snprintf(buffer, bufferSize, "%s", Menu::menuText[first]);
+  } else {
+    snprintf(buffer, bufferSize, "%s - %s", Menu::menuText[first], Menu::menuText[second]);
+  }
+}
+
 void Menu::show(int center_index) {
   u8g2.clearBuffer();
 
-  // Previous item
   u8g2.setFont(u8g2_font_6x10_tr);
-  u8g2.drawStr(0, 10, menuOptions[(center_index - 1 + menuSize) % menuSize]);
+
+  // Previous item
+  uint8_t prev_index = (center_index - 1 + menuSize) % menuSize;
+  char prevBuffer[32];
+  formatMenuOption(prev_index, prevBuffer, sizeof(prevBuffer));
+  u8g2.drawStr(0, 10, prevBuffer);
 
   // Selected item
   u8g2.setFont(u8g2_font_9x15_tr);
-  u8g2.drawStr(0, 23, menuOptions[center_index]);
+  char currBuffer[32];
+  formatMenuOption(center_index, currBuffer, sizeof(currBuffer));
+  u8g2.drawStr(0, 23, currBuffer);
 
   // Next item
   u8g2.setFont(u8g2_font_6x10_tr);
-  u8g2.drawStr(0, 32, menuOptions[(center_index + 1) % menuSize]);
+  uint8_t next_index = (center_index + 1) % menuSize;
+  char nextBuffer[32];
+  formatMenuOption(next_index, nextBuffer, sizeof(nextBuffer));
+  u8g2.drawStr(0, 32, nextBuffer);
 
   u8g2.sendBuffer();
 }
--- workstation/menu.h
+++ workstation/menu.h
@@ -1,18 +1,29 @@
 #ifndef MENU_H
 #define MENU_H
+
 #include <Wire.h>
 #include <U8g2lib.h>
 
 
 class Menu{
     private:
-        static constexpr const char* menuOptions[] = { "first option", "second option", "third option", "fourth option", "fifth option" };
-        static constexpr uint8_t menuSize = sizeof(menuOptions) / sizeof(menuOptions[0]);
+        static constexpr const char* menuText[] = {"Desktop", "Laptop", "No screen"};
+        // Items to show in the menu, indexes of menuText
+        // 255 means unset
+        static constexpr uint8_t menuOptions[][2] = {
+            {0, 255},
+            {1, 255},
+            {0, 1},
+            {1, 0},
+            {2, 255}
+        };
         static U8G2_SSD1306_128X32_UNIVISION_F_HW_I2C u8g2;
+        static void Menu::formatMenuOption(uint8_t index, char* buffer, size_t bufferSize);
 
     public:
         static void  show(int center_index);
         static void begin();
+        static constexpr uint8_t menuSize = sizeof(menuOptions) / sizeof(menuOptions[0]);
 };
 
 
